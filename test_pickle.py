7df30e3eee8605d51d8285872f970eae503ffa50
hupratt <hprattdo@gmail.com>
2023-05-04 17:33:50 +0200

    add test

--- test_pickle.py
+++ test_pickle.py
@@ -3,5 +3,6 @@ invite_sent_list = []
 with open("invite_sent_list.pkl", "rb") as file:
 	invite_sent_list = pickle.load(file)
 
+print(len(invite_sent_list))
 for person in invite_sent_list:
 	print(person.user_id)
