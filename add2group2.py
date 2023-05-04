f14dda5db4c7c25022e03003759de52c595dd3fa
hupratt <hprattdo@gmail.com>
2023-05-04 20:25:31 +0200

    remove sleep

--- add2group2.py
+++ add2group2.py
@@ -108,7 +108,6 @@ print(gr+"Loaded {} bans".format(len(invite_sent_list)))
 for user in users:
 	n += 1
 	result = None
-	time.sleep(random.randrange(5, 10))
 	try:
 		print(gr+"Trying to add id {} and hash {} to group {}".format(user['id'], user['access_hash'], target_group_entity))
 		if mode == 1 and str(user['id']) not in invite_sent_list:
