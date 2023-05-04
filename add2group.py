dbe0893e087cf51eed6cae08fd8fe6bd6e6968f9
hupratt <hprattdo@gmail.com>
2023-05-04 17:29:55 +0200

    gitignore

--- add2group.py
+++ add2group.py
@@ -98,6 +98,7 @@ print(gr+"Loaded {} bans".format(len(invite_sent_list)))
 
 for user in users:
 	n += 1
+	time.sleep(random.randrange(5, 10))
 	try:
 		print(gr+"Trying to add id {} and hash {} to group {}".format(user['id'], user['access_hash'], target_group_entity))
 		if mode == 1 and user not in invite_sent_list:
